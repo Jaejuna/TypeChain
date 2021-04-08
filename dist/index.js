076fb9df1a7c183faeb8d45f5730684a67c76851
jaejuna <anayana9988@gmail.com>
2021-04-08 17:59:07 +0900

    file structure changed

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
-const Suin = new Human("Su-in", 21, "female");
-const JJJ = new Human("jajuna", 23, "male");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-console.log(sayHi(Suin));
 //# sourceMappingURL=index.js.map
---
+++ src/Practice.ts
@@ -0,0 +1,23 @@
+//interface doesn't pass to js after compile but class does
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name:string, age: number, gender: string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const Suin = new Human("Su-in", 21, "female");
+const JJJ = new Human("jajuna", 23, "male");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(Suin));
+
+export {};
