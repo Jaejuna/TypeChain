ebed117315babb29fa61025a95d0f1da7cf4517b
jaejuna <anayana9988@gmail.com>
2021-04-08 17:33:57 +0900

    learning interface & class in ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hellO");
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name:string, age: number, gender: string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const Suin = new Human("Su-in", 21, "female");
+const JJJ = new Human("jajuna", 23, "male")
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(Suin));
+
+export {};
